5bf55e882609d76482024b357426dc488b502bb5
Kevin Fischer <kevin.fischer@fh-bielefeld.de>
2023-02-02 12:32:11 +0100

    [=] Fixed variable name in AdminSearch.

--- src/searchAdmins.py
+++ src/searchAdmins.py
@@ -16,7 +16,7 @@ class AdminSearch:
             raise Exception(f"{Logger.ERROR[0]}Error: You need to compare lists.{Logger.ENDL[0]}")
         res = []
         for set in data[0]:
-            if set not in data[1] and set.split(",")[2][0] == "w" and "_adm" not in set.split(",")[2] and set.split(",")[1] in admin_values:
+            if set not in data[1] and set.split(",")[2][0] == "w" and "_adm" not in set.split(",")[2] and set.split(",")[1] in admin_names:
                 res.append(set)
         return res
     
