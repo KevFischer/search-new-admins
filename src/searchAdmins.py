7d23430be87929b0f542a857741ccd8f9d6ccbf6
Kevin Fischer <kevin.fischer@fh-bielefeld.de>
2023-02-02 13:01:27 +0100

    [=] Added russian administrator name manually, because configparser can not handle characters.

--- src/searchAdmins.py
+++ src/searchAdmins.py
@@ -16,7 +16,7 @@ class AdminSearch:
             raise Exception(f"{Logger.ERROR[0]}Error: You need to compare lists.{Logger.ENDL[0]}")
         res = []
         for set in data[0]:
-            if set not in data[1] and set.split(",")[2][0] == "w" and "_adm" not in set.split(",")[2] and set.split(",")[1] in admin_names:
+            if set not in data[1] and set.split(",")[2][0] == "w" and "_adm" not in set.split(",")[2] and set.split(",")[1] in admin_names or set.split(",")[1] == "Пользователи":
                 res.append(set)
         return res
     
